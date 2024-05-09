fc3b7a6c81bd770d2686d93febc4a8bf9018d6ba
foxy242 <114094487+foxy242@users.noreply.github.com>
2024-05-09 12:28:03 +0300

    1

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const movies = [
     {
-        poster: './src/168914475526724638.png',
+        poster: './src/завантаження (2).png',
     },
     {
-        poster: './src/1686688401173211649.jpg',
+        poster: './src/завантаження (3).png',
     },
     {
         poster: './src/завантаження (1).png',
-    },
-    {
-        poster: './src/завантаження.png',
     },
     {
         poster: './src/завантаження.png',
@@ -30,14 +27,14 @@ const movies = [
         poster: './src/images.png',
     },
 ];
-const moviesListEl = document.querySelector('.movies__list');
+const moviesListEl = document.querySelector('.movies_list');
 const backdrop = document.querySelector('.backdrop');
 const modal = document.querySelector('.modal');
 
 movies.forEach(movie => moviesListEl.innerHTML += 
-<li>
-    <img class="movies__img" src="${movie.poster}" alt="poster ${movie.name}"/>
-</li>
+`<li>
+    <img class="movies_img" src="${movie.poster}" alt="poster ${movie.name}"/>
+</li>`
 );
 
 backdrop.addEventListener('click', () => backdrop.classList.add('hidden'))
