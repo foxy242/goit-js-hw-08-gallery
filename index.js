32333c572916ca9bb702a94f7c157b2995691ad5
foxy242 <114094487+foxy242@users.noreply.github.com>
2024-05-11 16:16:24 +0300

    1

--- index.js
+++ index.js
@@ -31,10 +31,28 @@ const moviesListEl = document.querySelector('.movies_list');
 const backdrop = document.querySelector('.backdrop');
 const modal = document.querySelector('.modal');
 
-movies.forEach(movie => moviesListEl.innerHTML += 
-`<li>
+movies.forEach(movie => moviesListEl.innerHTML += `
+<li class="movies_item" data-name="${movie.name}">
+    
     <img class="movies_img" src="${movie.poster}" alt="poster ${movie.name}"/>
-</li>`
-);
+    
+</li>
+`);
+
+moviesListEl.addEventListener('click', (e) => {
+    const findMovie = e.target.closest('.movies_item') === null ? false : e.target.closest('.movies_item').dataset.name;
+    if (findMovie) {
+        const movie = movies.find(movie => movie.name === findMovie);
+        backdrop.classList.remove('hidden')
+        modal.innerHTML = `
+            <h2 class="modal_title">${movie.name}</h2>
+            <p class="modal_year">Year: ${movie.year}</p>
+            <p class="modal_storyline">Storyline: ${movie.stotyline}</p>
+        `
+    }
+});
 
 backdrop.addEventListener('click', () => backdrop.classList.add('hidden'))
+
+// <h2 class="movies_title">${movie.name}</h2>
+// <p class="movies_rate">${movie.rate}/10</p>
